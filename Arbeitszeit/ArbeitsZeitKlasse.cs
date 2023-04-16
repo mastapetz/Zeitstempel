f68d47c4067412435276b3ac03856368d5e504b1
Peter Aigner <mastapetz@icloud.com>
2023-04-16 18:40:42 +0200

    Add project files.

---
+++ Arbeitszeit/ArbeitsZeitKlasse.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbeitszeit
+{
+    internal class ArbeitsZeitKlasse
+    {
+        //Anfangszeit
+        public DateTime _start;
+        public DateTime Start
+        {
+            get; set;
+        }
+
+
+        //Endzeit
+        public DateTime _end;
+        public DateTime End
+        {
+            get { return _end; }
+            set
+            {
+                //if (_start > value)
+
+                if (_start.Hour > _end.Hour)
+                {
+                    throw new ArgumentException("Falsche Zeiteingabe");
+                }
+                _end = value;
+            }
+        }
+        //Zeitspanne
+
+        internal TimeSpan _zeit;
+        internal TimeSpan Zeit { get; }
+
+        //Konstruktor
+
+        internal ArbeitsZeitKlasse(DateTime startZeit, DateTime endZeit)
+        {
+            _start = startZeit;
+            _end = endZeit;
+            _zeit = _end - _start;
+            if (_zeit > TimeSpan.Parse("6:30"))
+            {
+                _zeit -= TimeSpan.Parse("0:30");
+            }
+
+        }
+
+        //ToString
+
+        public override string ToString()
+        {
+            return $"Eingangszeit: {_start}  Endzeit: {_end} Dauer: {_zeit}";
+        }
+
+    }
+}
---
+++ Arbeitszeit/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Arbeitszeit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ArbeitsZeitKlasse ZeitStempelEins = new(DateTime.Now.AddHours(-7), DateTime.Now);
+            //Zeit._end = DateTime.Now;
+            //Zeit._start = Zeit._end.AddHours(-7);
+
+            //unter 6.5 stunden
+            DateTime eingangStempel1 = DateTime.Parse("9:00");
+            DateTime ausgangsStempel1 = DateTime.Parse("15:00");
+            ArbeitsZeitKlasse StempelPaar1 = new(eingangStempel1, ausgangsStempel1);
+            Console.WriteLine(StempelPaar1);
+            //*******************
+            Console.WriteLine("----------------------------------");
+            //*******************
+            //über 6.5 stunden
+            DateTime eingangStempel2 = DateTime.Parse("9:00");
+            DateTime ausgangsStempel2 = DateTime.Parse("19:00");
+            ArbeitsZeitKlasse StempelPaar2 = new(eingangStempel2, ausgangsStempel2);
+            Console.WriteLine(StempelPaar2);
+            //*******************
+            Console.WriteLine("----------------------------------");
+            //*******************
+            //falsche Zeit
+            DateTime eingangStempel3 = DateTime.Parse("11:00");
+            DateTime ausgangsStempel3 = DateTime.Parse("9:00");
+            ArbeitsZeitKlasse StempelPaar3 = new(eingangStempel3, ausgangsStempel3);
+            Console.WriteLine(StempelPaar3);
+
+
+        }
+    }
+}
