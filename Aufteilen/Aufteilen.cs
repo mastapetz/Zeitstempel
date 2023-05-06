8c1bdbc38c277dc1bd518e53fa9642200b4cc2f2
Peter Aigner <mastapetz@icloud.com>
2023-05-07 00:59:56 +0200

    HÜ4 vers1 not working

---
+++ Aufteilen/Aufteilen.cs
@@ -0,0 +1,49 @@
+﻿/*
+
+// Eine Runde Freunde macht gemeinsam einen Urlaub
+// Dabei zahlen einzelne immer für die ganze Gruppe
+// Jeder der etwas zahlt, schreibt die Zahlung mit und addiert am Ende des
+// Urlaubes seine Ausgaben zu einer Gesamtsumme.
+// Am Ende des Urlaubes wird ein Textfile erstellt, das die
+// Namen und die Summe der Ausgaben pro Person enthält
+// 
+// Aufgabe:
+
+// - Einlesen des Textfiles (Format kann selbst gewählt werden)
+// - Berechung eines Durchschnitts
+// - Aufteilen der Ausgaben zwischen den Personen,
+
+//   so dass nach der Aufteilung alle ausgeglichen sind
+
+// - Für die Implementierung können beliebige Datenstrukturen gewählt werden
+
+// Liste mit Personen
+// Pro Person gibt es eine Liste von Einträgen die er anderen schuldet
+
+// Beispiel:
+// Mittel: 725
+//* Name: Peter Betrag am Anfang: 1000
+//* Name: Mary Betrag am Anfang: 1500
+//* Name: Fritz Betrag am Anfang: 100, An Peter Betrag schuldig: 275, an Mary Betrag schuldig: 350
+//* Name: Franz Betrag am Anfang: 300, An Mary Betrag schuldig: 425
+
+
+ */
+namespace Aufteilen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"../../../urlaub.txt";
+
+            var urlaub = Personen.ParseTXT(path);
+
+            Console.WriteLine(urlaub);
+
+
+
+
+        }
+    }
+}
---
+++ Aufteilen/Personen.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aufteilen
+{
+    internal class Personen
+    {
+        public Dictionary<string, double> Schulden;
+
+        ////Properties
+        public string Name { get; set; }
+        public double Betrag { get; set; }
+
+        // Konstruktor
+        //public Personen(string name, double betrag)
+        //{
+        //    this.Name = name;
+        //    this.Betrag = betrag;
+        //}
+
+        internal static Dictionary<string, double> ParseTXT(string path)
+        {
+            // Dictionary anlegen
+            var Schulden = new Dictionary<string, double>();
+
+            // File Einlesen
+            string[] lines = File.ReadAllLines(path);
+
+            // Gesamtausgaben Berechnen
+            double gesamtAusgaben = 0;
+            foreach (string line in lines)
+            {
+                // Aufteilen
+                string[] parts = line.Split(';');
+                double betrag = double.Parse(parts[0]);
+                gesamtAusgaben += betrag;
+            }
+
+            // Durchschnitt berechnen
+            double durchschnitt = gesamtAusgaben / lines.Length;
+
+            // Ausgaben pro person Berechnen 
+            foreach (string line in lines)
+            {
+                // Aufteilen
+                string[] parts = line.Split(';');
+                string name = parts[1];
+                double betrag = double.Parse(parts[0]);
+
+                double ausgaben = betrag - durchschnitt;
+
+                // Ins dictionary speichern
+                if (Schulden.ContainsKey(name))
+                {
+                    Schulden[name] += ausgaben;
+                }
+                else
+                {
+                    Schulden.Add(name, ausgaben);
+                }
+
+            }
+
+
+
+            return Schulden;
+        }
+
+        public override string ToString()
+        {
+            string result = $"Name: {Name} Betrag {Betrag} Schulden {Schulden}";
+            foreach (var derp in Schulden)
+            {
+                result += $"\t{derp.Key}: {derp.Value}";
+            }
+
+            return result;
+        }
+
+
+    }
+}
